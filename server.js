1c83016346dcb2cffc9c566eae151ebbb23cee22
SeanW <sean.walmer@outlook.com>
2020-07-28 21:14:59 -0400

    added mlab info to server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds137862.mlab.com:37862/heroku_t5pxtk87");
 
 // Start the API server
 app.listen(PORT, function() {
